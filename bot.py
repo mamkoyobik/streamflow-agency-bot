2b8e125113e7ea2e1e66f0649810d0a2b065f902
yeashak <admin@MacBook-Air-Admin.local>
2026-02-03 08:11:48 +0300

    Fix aiogram import for 3.0.0

--- bot.py
+++ bot.py
@@ -11,7 +11,10 @@ from aiogram.types import (
     InputMediaPhoto, InputMediaVideo,
     ChatJoinRequest
 )
-from aiogram.client.default import DefaultBotProperties
+try:
+    from aiogram.client.default import DefaultBotProperties
+except Exception:
+    DefaultBotProperties = None
 from aiogram.enums import ParseMode, ChatAction
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
@@ -66,10 +69,16 @@ logger = logging.getLogger(__name__)
 
 # ================= BOT =================
 
-bot = Bot(
-    token=BOT_TOKEN,
-    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
-)
+if DefaultBotProperties:
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+else:
+    bot = Bot(
+        token=BOT_TOKEN,
+        parse_mode=ParseMode.HTML
+    )
 
 dp = Dispatcher(storage=MemoryStorage())
 
